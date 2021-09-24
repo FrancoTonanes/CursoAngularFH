89237af6a63bc641a9fbf56769f10641055f52c7
Franco adrian <adriantonanes@gmail.com>
2021-09-24 02:07:44 -0300

    Api gif

--- 02-gifsApp/src/app/gifs/busqueda/busqueda.component.ts
+++ 02-gifsApp/src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -6,11 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent  {
 
-  constructor() { }
+  @ViewChild('textBuscar') textBuscar !: ElementRef<HTMLInputElement>; //el "!" le dice a typescript que obvie la inferencia de tipos(solo usar cuando sabes que nunca será null)
 
-  ngOnInit(): void {
+  constructor( private gifsService : GifsService){
+
   }
+  buscar(){
+
+    const valor = this.textBuscar.nativeElement.value;
+
+    if ( valor.trim().length === 0 ){
+      return;
+    }
+
+    this.gifsService.buscarGifs( valor );
+
+  }
+
 
 }
--- 02-gifsApp/src/app/shared/sidebar/sidebar.component.ts
+++ 02-gifsApp/src/app/shared/sidebar/sidebar.component.ts
@@ -1,11 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
+
 export class SidebarComponent  {
+
+  constructor( private gifsService : GifsService ){
+  }
+
+  //SE DEBE CREAR OTRO GET PARA OBTENER LOS DATOS DE UN SERVICIO
+  get historial(){
+    return this.gifsService.historial;
+  }
+
+  buscar( termino : string ){
+    this.gifsService.buscarGifs( termino );
+  }
+
 
 
 }
